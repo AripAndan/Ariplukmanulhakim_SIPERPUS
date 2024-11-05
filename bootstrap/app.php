8d3f92ebe724881b1b375f3bc6d6abd5b797be68
Rifalhdri <rifalhdri@gmail.com>
2024-11-05 08:47:10 -0800

    Pertemuan ke-5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'role' => \spatie\Permission\Middleware\RoleMiddleware:: class,
+            'permission' => \spatie\Permission\Middleware\PermissionMiddleware:: class,
+            'role_or_permission' => \spatie\Permission\Middleware\RoleOrPermissionMiddleware:: class,
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
